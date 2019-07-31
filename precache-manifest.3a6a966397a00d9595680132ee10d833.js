a306957b24c736ccc7adc31d3bde85769b4b6328
reyanrd93 <yylee.rey@gmail.com>
2019-07-31 10:38:41 +0800

    Updates

---
+++ precache-manifest.3a6a966397a00d9595680132ee10d833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f745c3f9cc74feec61ddd31420906c6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9edd4e3203d3e3e5bea",
+    "url": "./static/css/main.84c5e013.chunk.css"
+  },
+  {
+    "revision": "e683de0090e27f098611",
+    "url": "./static/js/2.377a1c55.chunk.js"
+  },
+  {
+    "revision": "b9edd4e3203d3e3e5bea",
+    "url": "./static/js/main.7a7f3bff.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "./static/media/27.1 burger-logo.b8503d26.png"
+  }
+]);
